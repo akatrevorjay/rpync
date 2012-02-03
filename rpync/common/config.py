6874636c2f51badf0b24d599e19a859e3ddfaf18
cramren <marc.goeldner@googlemail.com>
2012-02-03 14:27:35 +0100

    changed user config

--- rpync/common/config.py
+++ rpync/common/config.py
@@ -3,7 +3,7 @@ import os.path
 from ConfigParser import SafeConfigParser as ConfigParser
 
 SYS_CONFIG_FILE = None
-USR_CONFIG_FILE = os.path.expanduser('~/.rpync.conf')
+USR_CONFIG_FILE = None
 DEF_CONFIG_PATH = os.path.dirname(__file__)
 DEF_CONFIG_FILE = None
 CFG             = None
@@ -22,11 +22,12 @@ def getConfigType():
     return CFG_TYPE
 
 def initConfig(config_type, config_file=None):
-    global CFG, CFG_TYPE, DEF_CONFIG_FILE, SYS_CONFIG_FILE
+    global CFG, CFG_TYPE, DEF_CONFIG_FILE, DEF_CONFIG_PATH, SYS_CONFIG_FILE, USR_CONFIG_FILE
     if CFG is None:
         if config_type in ('agent', 'client', 'server'):
             DEF_CONFIG_FILE = os.path.join(DEF_CONFIG_PATH, "default-{0}.conf".format(config_type))
             SYS_CONFIG_FILE = "/etc/rpync/{0}.conf".format(config_type)
+            USR_CONFIG_FILE = os.path.expanduser("~/.rpync-{0}.conf".format(config_type))
             CFG_TYPE        = config_type
         else:
             raise ValueError
